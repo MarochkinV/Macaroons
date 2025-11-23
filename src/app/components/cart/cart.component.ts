cf5d21cc172759f55debd625fa3e633c8928ba87
Marochkin Vladimir <vmarochkinv@gmail.com>
2025-11-23 19:23:10 +0500

    Added cart

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cart-component',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss']
+})
+export class CartComponent implements OnInit {
+  showCart: boolean = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
