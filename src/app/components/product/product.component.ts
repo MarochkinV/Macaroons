eac56208ba12d0d33e70c8028daff8d6aab9d143
Marochkin Vladimir <vmarochkinv@gmail.com>
2025-11-26 10:54:35 +0500

    Component made advantage

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ProductType} from "../../types/product.type";
+
+@Component({
+  selector: 'product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  @Input() product: ProductType;
+
+  constructor() {
+    this.product = {
+      image: '',
+      title: '',
+      amount: '',
+      price: '',
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
